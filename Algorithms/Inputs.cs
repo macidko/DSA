9e81a608a481a46ce741cf5eea435b32aea54265
Eren Macit <ernmctt@gmail.com>
2024-07-09 22:26:03 +0300

    Diagonal Difference

---
+++ Algorithms/Inputs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Inputs
+    {
+        // Diagonal Difference Input
+        public static void DiagonalDifferenceInput()
+        {
+            List<List<int>> arr = new List<List<int>>
+            {
+                new List<int> {11, 2, 4},
+                new List<int> {4, 5, 6},
+                new List<int> {10, 8, -12}
+            };
+
+            int result = Solutions.DiagonalDifference(arr);
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diagonal Difference
+            Inputs.DiagonalDifferenceInput();
+        }
+    }
+}
+
